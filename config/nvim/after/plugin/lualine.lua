574b2518dda1c255f9a0f2da6b13370553492102
Thomas Le Duc <dev@tleduc.de>
2021-07-16 18:15:37 +0200

    Show path for files in status bar

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -11,8 +11,10 @@ lualine.setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_b = {},
+    lualine_c = {
+        { 'filename', path = 1 }
+    },
     lualine_x = {
         { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
         'encoding',
@@ -24,7 +26,9 @@ lualine.setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {
+        { 'filename', path = 1 }
+    },
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
